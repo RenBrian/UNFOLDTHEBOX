2b81c73d528cf8bc6115d56b257e2d95cd98aad3
141250103 <141250103@smail.nju.edu.cn>
2018-08-14 22:32:14 +0800

    "08-14"

--- onebox/src/main/java/com/onebox/backend/controller/TestController.java
+++ onebox/src/main/java/com/onebox/backend/controller/TestController.java
@@ -2,22 +2,47 @@ package com.onebox.backend.controller;
 
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestController {
+	//测试列表
+	@RequestMapping("/list")
+	public String testList() {
+		return "/test/list";
+	}
+	//某个测试的详细信息
+	@RequestMapping("/detail")
+	public String testDetail() {
+		return "/test/detail";
+	}
 	
-	@RequestMapping("/genTest") 
-	public String genTest(@RequestBody Map<String, Object> reqMap) {
+	//新建测试
+	@RequestMapping("/generate")
+	public String testGenerate() {
+		return "/test/generate";
+	}
+	//生成测试的实现 - 不同的方式 - 接口实现
+	//Exercise平时练习(题库中提取) / Exam考试习题（导入） / 错题回顾（数据库中提取）
+	@RequestMapping("/genTestbyLib") 
+	public @ResponseBody String genTestbyLib(@RequestBody Map<String, Object> reqMap) {
 		//学科 mandarin / math
-//		String subject = reqMap.get("subject").toString();
-//		//Exercise平时练习(题库中提取) / Exam考试习题（导入） / 错题回顾（数据库中提取）
-//		String type = reqMap.get("type").toString();
-//		int ammount = Integer.parseInt(reqMap.get("ammount").toString());
-		
+		String subject = reqMap.get("subject").toString();
+		int ammount = Integer.parseInt(reqMap.get("ammount").toString());
+		return subject + ammount;
+	}
+	//导入试题
+	@RequestMapping("/genTestbyImport")
+	public @ResponseBody String genTestbyImport(@RequestBody Map<String, Object> map) {
+		return "";
+	}
+	//错题回顾
+	@RequestMapping("/genTestbyHistory")
+	public @ResponseBody String genTestbyHistory(@RequestBody Map<String, Object> map) {
 		return "";
 	}
 	
